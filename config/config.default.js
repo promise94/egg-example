e3c54190787740429dceae236e08361e5dc2fc65
lixiaowei <promise9495@163.com>
2019-07-17 20:54:17 +0800

    增加wanweiyiyuan

--- config/config.default.js
+++ config/config.default.js
@@ -1,3 +1,6 @@
+const appid = 100264;
+const secret = "f17fea13dd554a238c5e501c161b8523";
+
 exports.keys = "xiaowei";
 
 exports.view = {
@@ -5,4 +8,11 @@ exports.view = {
   mapping: {
     ".tpl": "nunjucks"
   }
+};
+
+exports.news = {
+  appid,
+  secret,
+  pageSize: 5,
+  serverUrl: "http://route.showapi.com/341-1"
 };
